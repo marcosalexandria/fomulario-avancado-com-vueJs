c0321f8baa289a51747b2667cfcc4d482b4eb0dd
Marcos Alexandria <93044961+marcosalexandria@users.noreply.github.com>
2022-02-22 21:20:03 -0300

    testefinal

---
+++ app/src/components/Main.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div v-for="pessoa in pessoa" :key="pessoa">
+            <p>Nome: {{pessoa.nome}}</p>
+            <p>Idade: {{pessoa.idade}}</p>
+        </div>
+
+        <form>
+            <div>
+                <input type="text" name="nome" id="nome" v-model="novaPessoa.nome">
+            </div>
+            <div>
+                <input type="number" name="idade" id="idade" v-model="novaPessoa.idade">
+            </div>
+            <div>
+                <button @click="add">Enviar</button>
+            </div>
+
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'Main',
+
+    data(){
+        return{
+           pessoa: [
+               {
+                   "nome":"Marcos",
+                   "idade": 24
+               },
+               {
+                   "nome":"Heloísa",
+                   "idade": 6
+               }
+           ],
+           novaPessoa:{}
+           
+        }
+    },
+    methods:{
+        add(e){
+            e.preventDefault();
+            this.pessoa.push(this.novaPessoa);
+        }
+    }
+}
+</script>
